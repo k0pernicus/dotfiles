4857d4a0e4f05625f145d82b760c1f6020d7358a
k0pernicus <antonin.carette@gmail.com>
2019-07-27 09:07:45 +0200

    Updated configuration files

--- setup_system.sh
+++ setup_system.sh
@@ -39,6 +39,8 @@ echo "> Installing Vim plug tool..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo "> Installing Vim plugins..."
 vim +PlugInstall +qall > /dev/null
+echo "> Updating YouCompleteMe libraries..."
+./~/.vim/plugged/YouCompleteMe/install.py --rust-completer
 echo "> Done!"
 
 # Zsh & OhMyZsh
