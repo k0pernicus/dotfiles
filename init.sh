d6f0b63f89233c46e5316a6d3cd4ebfd0803cc62
Antonin Carette <antonin@carette.xyz>
2020-03-07 20:56:08 +0100

    Added init script for both Linux + darwin systems

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Launch the setup according to the detected OS type
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	echo "os with type $OSTYPE found - setup for debian..."
+	sh setup/setup_system_debian.sh
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	echo "os with type $OSTYPE found - setup for macos..."
+  	sh setup/setup_system_darwin.sh
+elif
+	echo "os with type $OSTYPE does not have setup config!"
+	exit 1
+fi
