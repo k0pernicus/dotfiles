6a6b7e2f84c5ed4ee6437ea92562161072526b6c
k0pernicus <dev@carette.xyz>
2026-05-23 22:24:06 +0200

    Updated nvim config (added swift, python debugger, removed odin)

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -74,7 +74,6 @@ return {
             vim.lsp.enable("gopls")
             vim.lsp.enable("lua_ls")
             vim.lsp.enable("nixd")
-            vim.lsp.enable("ols")
             vim.lsp.enable("rust_analyzer")
             vim.lsp.enable("sourcekit")
 
--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -5,8 +5,11 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+			vim.keymap.set("n", "<leader>fa", function()
+				builtin.find_files({ hidden = true, no_ignore = true })
+			end, { desc = "Telescope find all files (including hidden / ignored" })
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 			vim.keymap.set("n", "<leader>fc", builtin.live_grep, { desc = "Telescope live grep" })
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 			vim.keymap.set(
